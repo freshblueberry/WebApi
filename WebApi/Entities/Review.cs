9a4cd10cff8cad593a6a68a38fc389b4859392b0
Clifford <choiclifford@gmail.com>
2021-03-21 04:24:03 -0700

    imported classes

---
+++ WebApi/Entities/Review.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+
+namespace WebApi.Entities
+{
+    public class Review
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ReviewID { get; set; }
+        public int ProductID { get; set; }
+        public string UserName { get; set; }
+        public int Rating { get; set; }
+        public string UserReview { get; set; }
+
+    }
+}
